5ca30ff8e598bd4df138cd93126e0f620e17a8ea
Matt Koski <maccam912@gmail.com>
2024-01-06 20:12:07 -0600

    Refactor UUID usage in test_client.py

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,5 +1,4 @@
 import pytest
-from uuid import UUID
 from workpluck_client.client import Client, TaskRetrievalResponse, ResultRetrievalResponse
 
 @pytest.fixture(scope='session')
@@ -7,7 +6,7 @@ def httpserver_listen_address():
     return ("127.0.0.1", 8080)
 
 def test_submit_task(httpserver):
-    task_id = str(UUID("0e6d41c2-0269-4466-b1aa-10ed25eee4d6"))
+    task_id = "0e6d41c2-0269-4466-b1aa-10ed25eee4d6"
     httpserver.expect_request("/task").respond_with_json({"id": task_id}, status=201)
 
     client = Client(httpserver.url_for("/"))
@@ -16,7 +15,7 @@ def test_submit_task(httpserver):
     assert str(response_task_id) == task_id
 
 def test_get_task(httpserver):
-    task_id = str(UUID("0e6d41c2-0269-4466-b1aa-10ed25eee4d6"))
+    task_id = "0e6d41c2-0269-4466-b1aa-10ed25eee4d6"
     httpserver.expect_request("/task").respond_with_json({"id": task_id, "input": {"key": "value"}}, status=200)
 
     client = Client(httpserver.url_for("/"))
